97731b40736a87d17b9abdb22c1bc89e0065f49a
Valerie0521 <58210509+Valerie0521@users.noreply.github.com>
2020-12-18 08:29:39 +0800

    Add files via upload
    初始页面生成代码

---
+++ pages/index/index.js
@@ -0,0 +1,80 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    /*pageBackgroundColor:'#5cb85c'*/
+  },
+  togra(){
+    wx.navigateTo({
+      url: '../choosegra/choosegra',
+    })
+  },
+  totype(){
+    wx.navigateTo({
+      url: '../choosetype/choosetype',
+    })
+  },
+  towrong(){
+    wx.navigateTo({
+      url: '../wrongset/wrongset',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
